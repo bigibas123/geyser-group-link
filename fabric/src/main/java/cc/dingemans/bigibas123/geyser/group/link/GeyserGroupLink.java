860c072c00c4fd6bba30449513016627e89586da
Bas Dingemans <bas@dingemans.cc>
2025-01-13 22:17:15 +0100

    Moved  fabric version of mod into it's own folder

--- fabric/src/main/java/cc/dingemans/bigibas123/geyser/group/link/GeyserGroupLink.java
+++ fabric/src/main/java/cc/dingemans/bigibas123/geyser/group/link/GeyserGroupLink.java
@@ -1,17 +1,17 @@
 package cc.dingemans.bigibas123.geyser.group.link;
 
 import eu.midnightdust.lib.config.MidnightConfig;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GeyserGroupLink implements ModInitializer {
+public class GeyserGroupLink implements DedicatedServerModInitializer {
     public static final String MOD_ID = "geyser-group-link";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitialize() {
+    public void onInitializeServer() {
         MidnightConfig.init(MOD_ID, GeyserGroupLinkConfig.class);
         ServerPlayConnectionEvents.JOIN.register(new PlayerJoinEventHandler(LOGGER));
         LOGGER.info("Initialized {}", MOD_ID);
